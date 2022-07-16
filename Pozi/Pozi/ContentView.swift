b6ca4aaf41f445b933665778571658a95702390a
Lee424 <tmdghks424@naver.com>
2022-07-16 14:24:29 +0900

    프로젝트 생성

---
+++ Pozi/Pozi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pozi
+//
+//  Created by seunghwan Lee on 2022/07/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pozi/Pozi/PoziApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoziApp.swift
+//  Pozi
+//
+//  Created by seunghwan Lee on 2022/07/16.
+//
+
+import SwiftUI
+
+@main
+struct PoziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

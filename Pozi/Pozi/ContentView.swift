d89fdce77259c0ac1f6a12a08f506f11dc1c04a6
Lee424 <tmdghks424@naver.com>
2022-07-16 17:32:05 +0900

    네이버 지도뷰 추가

--- Pozi/Pozi/ContentView.swift
+++ Pozi/Pozi/ContentView.swift
@@ -9,8 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        MapView(coord: (33.361425, 126.529418))
     }
 }
 
--- Pozi/Pozi/MapView/MapView.swift
+++ Pozi/Pozi/MapView/MapView.swift
@@ -5,4 +5,26 @@
 //  Created by seunghwan Lee on 2022/07/16.
 //
 
-import Foundation
+import SwiftUI
+import NMapsMap
+
+struct MapView: UIViewRepresentable {
+    var coord: (Double, Double)
+    
+    func makeUIView(context: Context) -> NMFNaverMapView {
+        let view = NMFNaverMapView()
+        view.showZoomControls = false
+        view.mapView.positionMode = .direction
+        view.mapView.zoomLevel = 17
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: NMFNaverMapView, context: Context) {
+        let coord = NMGLatLng(lat: coord.1, lng: coord.0)
+        let cameraUpdate = NMFCameraUpdate(scrollTo: coord)
+        cameraUpdate.animation = .fly
+        cameraUpdate.animationDuration = 1
+        uiView.mapView.moveCamera(cameraUpdate)
+    }
+}
